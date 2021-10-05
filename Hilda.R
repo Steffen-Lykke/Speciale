47d432cce8f6361e6a21396114908bd23c39f789
Steffen Lykke <slch17@student.aau.dk>
2021-10-05 13:17:24 +0200

    plot af hilda plus lidt lækker kode der selv finder nogenlunde frem til hvornår forsøget er startet

--- Hilda.R
+++ Hilda.R
@@ -20,13 +20,33 @@ library(latex2exp)
 library(zoo)
 
 ################ Hent Data ##################
-#test= read.csv("data/CWF_12.4.2021_07-09-2021 084249.csv")
-CWF <- read_delim("data/Hilda/CWF_12.4.2021_23-09-2021 103733.csv", 
+
+hilda_1 <- read_delim("data/Hilda/hilda_1.csv", 
                   delim = "\t", escape_double = FALSE, 
                   trim_ws = TRUE, skip = 5) #Hente data og få det til passe ned i en tabel
+
+hilda_2 <- read_delim("data/Hilda/hilda_2.csv", 
+                      delim = "\t", escape_double = FALSE, 
+                      trim_ws = TRUE, skip = 5) #Hente data og få det til passe ned i en tabel
+
+hilda_3 <- read_delim("data/Hilda/hilda_3.csv", 
+                      delim = "\t", escape_double = FALSE, 
+                      trim_ws = TRUE, skip = 5) #Hente data og få det til passe ned i en tabel
+
+hilda_4 <- read_delim("data/Hilda/hilda_4.csv", 
+                      delim = "\t", escape_double = FALSE, 
+                      trim_ws = TRUE, skip = 5) #Hente data og få det til passe ned i en tabel
+
+hilda_5 <- read_delim("data/Hilda/hilda_5.csv", 
+                      delim = "\t", escape_double = FALSE, 
+                      trim_ws = TRUE, skip = 5) #Hente data og få det til passe ned i en tabel
+
+hilda_6 <- read_delim("data/Hilda/hilda_6.csv", 
+                      delim = "\t", escape_double = FALSE, 
+                      trim_ws = TRUE, skip = 5) #Hente data og få det til passe ned i en tabel
 #str(CWF)
-dat = as.data.frame(CWF) #Ændrer strukturen af dataen til noget vi kan arbejde med
-old_names=colnames(CWF) #Vi finder de latterlige navne kolonnerne i dataen har 
+dat = rbind(as.data.frame(hilda_2),as.data.frame(hilda_3),as.data.frame(hilda_4),as.data.frame(hilda_5),as.data.frame(hilda_6)) #Ændrer strukturen af dataen til noget vi kan arbejde med
+old_names=colnames(hilda_1) #Vi finder de latterlige navne kolonnerne i dataen har 
 new_names=c("time","level","con0201","pH0201","con0301","flow0301",
             "p0301","p0302","p0401","flow0401","p0501","con0501",
             "temp0501","p0601","temp0601","16","17","18","19","20",
@@ -37,18 +57,29 @@ new_names=c("time","level","con0201","pH0201","con0301","flow0301",
 dat = dat %>% rename_at(all_of(old_names),~new_names) #Vi ændrer navnene til de nye
 dat$time = dmy_hms(dat$time)#Vi ændrer formatet af 'dato' kolonnen til et standard format for tid og datoer istedet for at det bare er noget tekst
 
+### Forsøg på automatisk at finde t=0
+
+start_tid = min(which(dat$flow0501>16&dat$flow0501<25))
+dat=dat[-c(1:start_tid),]
+### Tid i s fra forsøgs start
 dat = dat%>%
    mutate(sek=(
-           as.numeric(as.POSIXct(time))-as.numeric(as.POSIXct("2021-09-14 11:16:26", format='%Y-%m-%d %H:%M:%S'))
+           as.numeric(as.POSIXct(time))-as.numeric(dat$time[1], format='%Y-%m-%d %H:%M:%S'))
                 ) #den regner 2 timer for meget, tag højde for det manuelt checket hvornår flow rammer 17 ml/min. 
-            )
+            
 
 
 #str(dat)
 ####################### Plotting of Data ##########################
-ggplot(dat,aes(time,p_avg0302)) + geom_line() + theme_bw() +
-  ylab("Pressure [Bar]") + xlab("Time") + 
-  scale_x_datetime(breaks = scales::date_breaks("30 mins"),date_labels = "%H:%M") #Et relativt simplet plot af noget tryk mod tid i ggplot, det er ikke så 'interaktivt' så vi prøver også med plotly
+ggplot(dat,aes(sek/3600,p_avg0302)) + geom_line() + theme_bw() +
+  ylab("Pressure [Bar]") + xlab("Time [h]")  
+  #Et relativt simplet plot af noget tryk mod tid 
+
+
+ggplot(dat,aes(sek/3600,flow0501)) + geom_line() + theme_bw() +
+  ylab("Pressure [Bar]") + xlab("Time [h]")+xlim(c(0,80))+ylim(c(0,20))  
+
+
 
 dat %>%#dplyr arbejder indenfor 'dat' data framen
   mutate(TMP=(p_avg0301+p_avg0302)/2-p_avg0501)%>%#Her regner vi TMP, som dplyr husker nu er en del af den data
