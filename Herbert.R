9dcef4a8a3e18bcd4e276af0f04684a5f4026a1d
Steffen Lykke <slch17@student.aau.dk>
2021-10-22 12:53:55 +0200

    CT Model

---
+++ Herbert.R
@@ -0,0 +1,128 @@
+#Der skal ske noget syg programmering her
+#CT model ud fra COC
+
+
+
+#### Initialize Packages ####
+library(dplyr)
+library(readr)
+library(tidyr)
+library(ggplot2)
+library(readxl)
+library(visdat)
+library(GGally)
+library(Hmisc)
+library(DMwR2)
+library(data.table)
+library(mltools)
+library(ggfortify)
+library(ggiraphExtra)
+library(coronavirus)
+library(lubridate)
+library(plotly)
+library(latex2exp)
+library(zoo)
+library(fcuk)
+
+
+
+##### CT parameters #####
+V_CT = 8 # m^3 | Hvad er reservoir volumet
+Q_vap = 2 #m^3 / day | Hvor meget fordamper
+Q_blowdown = 1 #m^3 /d hvor meget fjernes fra resevooiret
+
+
+
+c_makeup = c(
+  1.6, #Na
+  0.6, #Cl
+  0.5, #SO4
+  0.4 #SiO2
+  ) #En vektor med de forskellige koncentrationer
+
+
+c_CT_ini = c_makeup
+
+
+c_guideline =c(
+  12, #Na
+  7.1, #Cl
+  2.5, #SO4
+  2.5 #SiO2
+) #Vektor med grænseværdier for ioner
+
+COC_max = c_guideline/c_makeup
+COC = min(COC_max)
+drift=0
+##### Model Parameters #####
+dt=60 #minutes
+run_time = 10 #days
+max_time = run_time*60*24 #i minutter
+n_time_step = max_time/dt+1
+tid = 0
+
+#### Data frames ####
+df = data.frame(
+  tid=double(),  
+  V_CT=double(),
+  stringsAsFactors = FALSE
+)
+
+nf = data.frame(
+  tid=double(),
+  Na=double(),
+  Cl=double(),
+  Ca=double(),
+  SO4=double()
+)
+
+cf = data.frame(
+  tid=double(),
+  Na=double(),
+  Cl=double(),
+  Ca=double(),
+  SO4=double()
+)
+#Initial Values
+df[1:n_time_step,]=0
+nf[1:n_time_step,]=0
+cf[1:n_time_step,]=0
+df[1,]=c(tid,V_CT)
+nf[1,]=c(tid,c_makeup)
+cf[1,]=nf[1,]/df$V_CT[1]
+###### CT Model ######
+for (i in 2:n_time_step) {
+  
+  ## Volume flow
+  Q_blowdown=Q_vap/(-1+COC)
+  Q_makeup = Q_vap + Q_blowdown # hvor meget vand skal ind i systemet
+  Q_vap = Q_vap #big math
+  Q = Q_makeup-Q_blowdown-Q_vap
+  ## Current volume
+  df$V_CT[i]=df$V_CT[i-1]+Q
+  
+  
+  ## mass flow ##
+  n_mu = Q_makeup*c_makeup
+  n_bd = Q_blowdown*cf[i-1,2:5]
+  n_vap = Q_vap*cf[i-1,2:5]*drift
+  n_flow=n_mu-n_bd-n_vap
+
+  ## Current Concentration ##
+
+  nf[i,2:5] = nf[i-1,2:5]+n_flow
+  cf[i,2:5] = nf[i,2:5]/df$V_CT[i]  
+
+  df$tid[i]=df$tid[i-1]+1
+  nf$tid[i]=nf$tid[i-1]+1
+  cf$tid[i]=cf$tid[i-1]+1
+  
+}
+
+########## plot #############
+cf.long = cf %>% 
+  gather(key,value, Na,Cl)
+
+ggplot(cf.long,aes(x=tid/60,y=value,color=key))+geom_line()+
+  scale_color_brewer(palette="Set1",labels = c("Na", "Cl"))+
+  theme_bw()+labs( y = "Concentration [mM?]", x = "Time [days]", color = "")
