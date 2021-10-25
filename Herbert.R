0a844758e41c4213bac5b08071ab189ee5ef4908
Steffen Lykke <slch17@student.aau.dk>
2021-10-25 09:43:17 +0200

    CT model, but is slower than I would like

--- Herbert.R
+++ Herbert.R
@@ -31,8 +31,6 @@ V_CT = 8 # m^3 | Hvad er reservoir volumet
 Q_vap = 2 #m^3 / day | Hvor meget fordamper
 Q_blowdown = 1 #m^3 /d hvor meget fjernes fra resevooiret
 
-
-
 c_makeup = c(
   1.6, #Na
   0.6, #Cl
@@ -40,12 +38,8 @@ c_makeup = c(
   0.4 #SiO2
   ) #En vektor med de forskellige koncentrationer
 
-
-c_CT_ini = c_makeup
-
-
 c_guideline =c(
-  12, #Na
+  6, #Na
   7.1, #Cl
   2.5, #SO4
   2.5 #SiO2
@@ -53,15 +47,20 @@ c_guideline =c(
 
 COC_max = c_guideline/c_makeup
 COC = min(COC_max)
-drift=0
+
 ##### Model Parameters #####
-dt=1 #minutes
+dt=10 #minutes
 dt=dt/1440
-run_time = 1 #days
+run_time = 100 #days
 max_time = run_time*60*24 #i minutter
 n_time_step = run_time/dt+1
 tid = 0
+drift = 0
 
+
+#df=mat.or.vec(n_time_step,2)
+#nf=mat.or.vec(n_time_step,5)
+#cf=nf
 #### Data frames ####
 df = data.frame(
   tid=double(),  
@@ -89,14 +88,13 @@ df[1:n_time_step,]=0
 nf[1:n_time_step,]=0
 cf[1:n_time_step,]=0
 df[1,]=c(tid,V_CT)
-nf[1,]=c(tid,c_makeup)
-cf[1,]=nf[1,]/df$V_CT[1]
+cf[1,]=c(tid,c_makeup)
+nf[1,]=cf[1,]*df$V_CT[1]
+Q_vap = Q_vap*dt #big math
 ###### CT Model ######
 for (i in 2:n_time_step) {
   
   ## Volume flow
-  
-  Q_vap = Q_vap*dt #big math
   Q_blowdown=(Q_vap/(-1+COC))
   Q_makeup = Q_vap + Q_blowdown # hvor meget vand skal ind i systemet
   Q = Q_makeup-Q_blowdown-Q_vap
@@ -117,17 +115,17 @@ for (i in 2:n_time_step) {
 
 
   df$tid[i]=df$tid[i-1]+dt
-  nf$tid[i]=nf$tid[i-1]+dt
-  cf$tid[i]=cf$tid[i-1]+dt
+  nf$tid[i]=df$tid[i]
+  cf$tid[i]=df$tid[i]
 
   
 }
 
 ########## plot #############
 cf.long = cf %>% 
-  gather(key,value, Na,Cl,SO4,SiO2)
+  gather(key,value,Na,Cl,SO4,SiO2)
 
-
-ggplot(cf.long,aes(x=tid,y=value,color=key))+geom_line()+
-  scale_color_brewer(palette="Set1",labels = c("Na", "Cl","SO4","SiO2"))+
-  theme_bw()+labs( y = "Concentration [mM?]", x = "Time [days]", color = "")
+level_order = c('Na','Cl','SO4','SiO2')
+ggplot(cf.long,aes(x=tid,y=value,color=factor(key,level=level_order)))+geom_line()+
+  scale_color_brewer(palette="Set1",labels = level_order)+
+  theme_bw()+labs(y = "Concentration [mM]", x = "Time [days]", color = "Ion")
