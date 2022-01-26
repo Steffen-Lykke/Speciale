14b4275bd395e920be8083d364295ac2a9648159
Steffen Lykke <slch17@student.aau.dk>
2022-01-26 14:39:39 +0100

    titreringsdata

--- Job/GBJ_Titering_event.R
+++ Job/GBJ_Titering_event.R
@@ -3,34 +3,61 @@ library(dplyr)
 library(stats)
 library(lubridate)
 library(rio)
-
-setwd("C:/Users/53887/OneDrive - Grundfos/Desktop/Titering/CSV")
+library(here)
+setwd(here("Job","IWR titrering"))
 
 columnnames <- c("mL","pH", "sec","dpHdml")
 
 #load af forskellige filer
-#alle test er kørt med 0,1M NaOH 
-#Test 1 : rå spildevand d. 25/10-21
-test_1 <- import("1_pH_titration_NaOH_22_11_21-11_11_51.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+#alle test er k?rt med 0,1M NaOH 
+#Test 1 : r? spildevand d. 25/10-21
+T2_11 <- import("11_nov_tank2_NaOH_0.1M_50mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
   mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
-#Test 2 : Udløbsspildevand d. 25/10-21
-test_2 <- import("2_igen_pH_titration_NaOH_19_11_21-15_09_44.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+T3_11 <- import("11_nov_tank2_NaOH_0.2M_50mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
   mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
-#Test 3 : rå spildevand d. 04/11-21
-test_3 <- import("3_pH_titration_NaOH_22_11_21-12_43_20.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+
+T2_15 <- import("15_nov_tank2_NaOH_0.1M_50mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
   mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
-#Test 4 : Udløbsspildevand d. 04/11-21
-test_4 <- import("4_pH_titration_NaOH_19_11_21-12_55_35.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+T3_15 <- import("15_nov_tank2_NaOH_0.1M_50mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
   mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
-#Test 5 : rå spildevand d. 05/11-21
-test_5 <- import("5_pH_titration_NaOH_22_11_21-14_01_59.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+
+T1_18 <- import("18_nov_tank1_NaOH_0.2M_50mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
   mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
-#Test 6 : Udløbsspildevand d. 05/11-21    udløbs turb=35 NTU
-test_6 <- import("6_pH_titration_NaOH_19_11_21-14_08_05.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+T2_18 <- import("18_nov_tank2_morgen_NaOH_0.2M_50mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+T3_18 <- import("18_nov_tank3_NaOH_0.1M_50mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+
+T1_19 <- import("19_nov_tank1_pH_NaOH_0.2M_46mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+T2_19 <- import("19_nov_tank2_NaOH_0.5_50mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+T3_19 <- import("19_nov_tank3_NaOH_0.2M_49mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+
+T1_23 <- import("23_nov_tank1_NaOH_0.5_100mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+T2_23 <- import("23_nov_tank2_pH_NaOH_0.2M_100mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+T3_23 <- import("23_nov_tank3_pH_NaOH_0.2M_100mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+
+T1_25 <- import("25_nov_tank1_pH_NaOH_0.2M_100mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+T2_25 <- import("25_nov_tank2_NaOH_0.5M_100mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+T3_25 <- import("25_nov_tank3_pH_NaOH_0.2M_100mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+
+T1_29 <- import("29_nov_tank1_pH_NaOH_0.2_M_100mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+T2_29 <- import("29_nov_tank2_pH_NaOH_0.2M_100mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
+  mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
+T3_29 <- import("29_nov_tank3_pH_NaOH_0.2M_100mL.csv",";", skip=1, header=F,stringsAsFactors = FALSE,col.names = columnnames) %>%
   mutate(mL = as.numeric(mL),pH = as.numeric(pH),sec = as.numeric(sec),dpHdml = as.numeric(dpHdml))
 
 #differentiering af data
-titrationdata <- test_3
+titrationdata <- T3_23
 n <- 7
 dvdpH <- diff(titrationdata$mL,lag=n)/diff(titrationdata$pH,lag=n) 
 length(dvdpH) =nrow(titrationdata)-(n-1)
@@ -41,12 +68,12 @@ na_df<-data.frame(matrix(NA,nrow = n-1, ncol = 2))
 colnames(na_df) <- c('dvdpH','dpHdv')
 diff.cal<-rbind(na_df,diff.cal)
 titrationdata <-cbind(titrationdata,diff.cal)
-write.csv(titrationdata,"C:/Users/53887/OneDrive - Grundfos/Desktop/Titering/Indlet 4-11.csv")
+#write.csv(titrationdata,here("Job","Indlet 4-11.csv"))
 
 #polynomisk regration
-tidata3<-dplyr::filter(titrationdata, dvdpH > -100 &dvdpH < 100  &pH<5.5&pH>3.3)
-tidata3$mod<-fitted(lm(tidata3$dvdpH ~ poly(tidata3$pH,2)))
-tidata3[which.max(tidata3$mod),]
+tidata1<-dplyr::filter(titrationdata, dvdpH > -100 &dvdpH < 100  &pH<5.5&pH>3.3)
+tidata1$mod<-fitted(lm(tidata1$dvdpH ~ poly(tidata1$pH,2)))
+tidata1[which.max(tidata1$mod),]
 
 
 #plot af data
@@ -65,16 +92,13 @@ P2 <- plot_ly(data = titrationdata, x = ~pH) %>%
                             spikedash = "solid", 
                             showline = TRUE), hovermode = "x",
                  spikedistance = -1) 
+
 P2
 
 
 pHplot <- plot_ly(data = titrationdata, x = ~pH) %>%
-  add_lines(y = test_1$pH, name = "pH test 1", x = test_1$mL,line=list(width=4))%>%
-  add_lines(y = test_2$pH, name = "pH test 2", x = test_2$mL,line=list(width=4))%>%
-  add_lines(y = test_3$pH, name = "pH test 3", x = test_3$mL,line=list(width=4))%>%
-  add_lines(y = test_4$pH, name = "pH test 4", x = test_4$mL,line=list(width=4))%>%
-  add_lines(y = test_5$pH, name = "pH test 5", x = test_5$mL,line=list(width=4))%>%
-  add_lines(y = test_6$pH, name = "pH test 6", x = test_6$mL,line=list(width=4))%>%
+  add_lines(y = T2_11$pH, name = "pH test 1", x = T2_11$mL,line=list(width=4))%>%
+  add_lines(y = T3_11$pH, name = "pH test 2", x = T3_11$mL,line=list(width=4))%>%
   plotly::layout(
     yaxis = list(title = "pH", showgrid = FALSE, color = "red"),
     xaxis=list(title="V [mL]"),
