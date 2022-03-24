e1d058fdfb5f0cb937b227cd0d661fba3567d883
Steffen <slch17@student.aau.dk>
2022-03-24 15:40:46 +0100

    Mere DSPM

--- Modeling/DSPM.R
+++ Modeling/DSPM.R
@@ -34,5 +34,65 @@ K_a=
 c_perm = (y_N*c_N*phi_S*phi_DE*phi_Don)/y_perm
 
 
+#### Nyt efter vi er blevet super kloge ########
+
+###### Basic data  ######
+ion_data = data.frame(
+  ion = c("Na", "Cl", "SO4", "SiO2","Ca","HCO3"),
+  Mw=c(23,35,96,60,40,61),
+  molar_con = c(50.1, 76.4, 160, NA,119,NA),# Molær konduktivitet[S*cm^2*mol^-1]
+  stokes=c(1.84,1.21,2.3,NA,3.1,NA)*0.1,
+  pauling=c(0.95,1.81,2.9,NA,0.99,NA)*0.1,
+  hydrated=c(3.58,3.32,3.82,NA,3.12,NA)*0.1,
+  z=c(1,-1,-2,-1,2,-1),
+  Diff=c(NA,NA,NA,NA,NA,NA)
+)
+
+feed = data.frame(
+  ion = c("Na", "Cl", "SO4", "SiO2","Ca","HCO3"),
+  concentration = c(15.25,5,5,1.25,0.5,NA)
+)
+###### Constants ######
+E_b = 78.4
+E_la = 31
+delta = 0.28 #nm
+e = 1.602*10^-19 #C
+E_0 = 8.85*10^-12 #F m^-1
+k_B = 1.3806*10^-23 #J/mol?
+Temp=25 #C
+Temp=Temp+273.15 #K
+Faraday = 96485 #C/mol
+R_gas = 8.314 #J/K/mol
+
+
+###### Membrane Parameters #####
+rp=0.5 #nm
+Le = 2 #um
+sigma=-1.2#mC m^-2
+
+
+###### Other Variables #####
+P=3#bar
+
+
+
+
+###### Prep ####
+radius="stokes"
+radii = ion_data%>%select(c(ion,radius,z))
+X=(2*sigma/1000)/(rp*10^-9*Faraday)#mmol
+
+##### Modeling start #####
+
+###### Steric exclusion #####
+steric=(1-(radii[,2]/rp))^2
+
+
+###### Dielectric exclsion #####
+E_p=E_la+(E_b-E_la)*(1-(delta/rp))^2
+dW = (radii$z^2*e^2)/(8*pi*E_0*radii[,2]*10^-9)*(1/E_p-1/E_b)
+DE = exp(-dW/(k_B*Temp))
+
+###### Donnan exclusion?#####
 
 
